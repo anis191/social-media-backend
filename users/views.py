6fa1929dc71ffe85563248f0b7e88312c1e13599
anisul alam <anisulalam2002@gmail.com>
2026-02-07 23:36:09 +0600

    feat(user-data): Secure user profile api

--- users/views.py
+++ users/views.py
@@ -1,12 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import *
 from .serializers import *
-
-# class UserViewSet(ModelViewSet):
-    # queryset = User.objects.all()
-    # serializer_class = UserSerializer
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
 
 class UserProfileViewSet(ModelViewSet):
+    http_method_names = ['get','put','patch']
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     lookup_field = 'slug'
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
